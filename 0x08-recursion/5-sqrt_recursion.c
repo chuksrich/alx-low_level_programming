13ec58a438696b5810a2fd7d436659a763728484
chuksrich <chuksrich2010@gmail.com>
2023-01-04 16:32:05 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,3 +22,22 @@ int _evaluate(int i, int n)
 
 	return (-1);
 }
+
+/**
+ * *_sqrt_recursion - evaluate sqrt
+ * *@n: integer
+ * *Return: Sqrt_recursion
+ * */
+
+int _sqrt_recursion(int n)
+[
+	int i = 0;
+
+	if (i < 0) /*if n is negative*/
+		return (-1);
+	
+	else
+	{
+		return (_evaluate(i, n)); /*Recursion call*/
+	}
+}
